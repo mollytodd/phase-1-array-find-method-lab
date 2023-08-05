7781b3c9c193c8e3d4e3fb83064e52ce971b3a3a
Molly Todd <mollyjt89@gmail.com>
2023-08-05 12:03:36 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function superbowlWin(recordData) {
+  const winningRecord = recordData.find((entry) => entry.result === "W");
+
+  if (winningRecord) {
+    return winningRecord.year;
+  } else {
+    return undefined;
+  }
+}
+
+const data = [
+  { year: "2016", result: "N/A" },
+  { year: "2017", result: "L" },
+  { year: "2018", result: "W" },
+  { year: "2019", result: "N/A" },
+  { year: "2020", result: "W" },
+];
+
+const superbowlWinner = superbowlWin(data);
+
+console.log(superbowlWinner);
+
